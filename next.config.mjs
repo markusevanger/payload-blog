ad6c1496e967c7fbb293fb1292e3411519b364f9
Markus <markusevanger@gmail.com>
2025-03-19 15:53:17 +0100

    add domain to next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,13 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+
+  images: {
+    domains: [
+      'feed.payload.markusevanger.no',
+      'localhost'
+    ],
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
